ad4991314484ac5665cd670e7a9592c34d2b9bd2
Julia Mathe <julia.mathe@cern.ch>
2022-08-31 16:48:55 +0200

    [DF] Add documentations and simplify code

--- tutorials/dataframe/tensorflow/make_model.py
+++ tutorials/dataframe/tensorflow/make_model.py
@@ -1,36 +1,40 @@
 import numpy as np
-import random
-
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
 from keras.layers.core import Dense
 from keras.models import Sequential
+from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
+
     
+# To test the TensorflowCEvaluator, we need to build a model first
+# Let's make an XOR gate using a sequential model
 model = Sequential()
+# This number of dense layers and input neurons shows good fitting performance later on
 model.add(Dense(32, input_dim=2, activation='relu'))
 model.add(Dense(32, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(optimizer="adam", loss="mean_squared_error", metrics=['binary_accuracy'])
 
-# XOR gate
-training_data = np.array([[0,0],[0,1],[1,0],[1,1]], "float32")
-target_data = np.array([[0],[1],[1],[0]], "float32")
+# The training data corresponds to the input and output logic of the XOR gate
+x_train = np.array([[0,0],[0,1],[1,0],[1,1]], "float32")
+y_train = np.array([[0],[1],[1],[0]], "float32")
 
-model.fit(training_data, target_data, epochs=100, verbose=2)
+# Since we want a "working" model, we need to train it first on our training data over several epochs
+model.fit(x_train, y_train, epochs=100, verbose=2)
 
+# Now we that we have a trained model, we want to save it in a pb file as a "frozen graph"
+# These steps are necessary to make the model readable by the TensorflowCEvaluator
+# Note that simply saving the model does not provide the right structure for the model to be read.
+# So we need to convert our Keras model into a TF Graph using tf.function
 full_model = tf.function(lambda x: model(x))
 full_model = full_model.get_concrete_function(
     tf.TensorSpec(model.inputs[0].shape, model.inputs[0].dtype))
-print(model.inputs[0])
-print(model.outputs[0])
 
-# Get frozen ConcreteFunction   
+# To produce a single pb file containing the full model including variables and hyperparameters,
+# we need to "freeze" our model
 frozen_func = convert_variables_to_constants_v2(full_model,lower_control_flow=False)
 frozen_func.graph.as_graph_def()
 layers = [op.name for op in frozen_func.graph.get_operations()]
-
-# Save frozen graph from frozen ConcreteFunction to hard drive
+# The last step is to save the frozen graph from the frozen Concrete Function to hard drive
 tf.io.write_graph(graph_or_graph_def=frozen_func.graph,
                   logdir="./frozen_models",
                   name="frozen_graph.pb",
--- tutorials/dataframe/tensorflow/tf_test.C
+++ tutorials/dataframe/tensorflow/tf_test.C
@@ -16,14 +16,39 @@ int main() {
 
     // Test the TensorflowCEvaluator with an easy XOR model
     const std::string& modelfile = "frozen_graph.pb";
+
+    // We must tell the TensorflowCEvaluator the input and output nodes
+    // so that it can correctly read the model's graph
     const std::vector<std::string>&  input = {"x"};
     const std::vector<std::string>&  output = {"Identity"};
+
+    // If you don't know the names of the input and output nodes of your model, 
+    // use this code to read them from your pb model:
+    /*
+    from google.protobuf import text_format
+    import tensorflow as tf
+    import sys
+
+    graph = tf.Graph()
+    with graph.as_default():
+        graph_def = graph.as_graph_def()
+
+        with tf.io.gfile.GFile(sys.argv[1], "rb") as f:
+            graph_def.ParseFromString(f.read())
+
+        tf.import_graph_def(graph_def)
+
+    print('\n'.join([op.name for op in graph.get_operations()]))
+    */
+
     TensorflowCEvaluator model (modelfile, input, output); 
 
+    // Make data to test the model
     std::vector<std::vector<int>> v_in{{0, 0}, {0, 1}, {1, 0}, {1, 1}}; 
-    std::vector<int> v_check{0, 1, 1, 0};
-    //int i = 0; int k = 0;
+    std::vector<int> v_check{0, 1, 1, 0}; // reference
 
+    // Store the data into the columns of a dataframe and compute another
+    // column with the model output 
     auto d = ROOT::RDataFrame(4)
         .Define("x", [&v_in](ULong64_t e) { return v_in[e]; }, {"rdfentry_"})
         .Define("output", [&model, &v_in](ULong64_t e){
